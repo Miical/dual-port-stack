0d5da76485c5108e990161c2f5d0434bc23b87be
Miical <879754743@qq.com>
2024-07-29 13:52:34 +0800

    Change to single port env

--- mlvp-driven.py
+++ mlvp-driven.py
@@ -14,33 +14,25 @@ class BusCMD(Enum):
     POP_OKAY = 3
 
 class StackEnv(Env):
-    def __init__(self, port0, port1):
-        super().__init__(monitor_step=port0.step)
-        self.port0 = port0
-        self.port1 = port1
+    def __init__(self, port):
+        super().__init__(monitor_step=port.step)
+        self.port = port
 
-    async def exec_once(self, port, is_push, data=0):
-        await ClockCycles(port, random.randint(0, 5))
-        port.in_valid.value = 1
-        port.in_cmd.value = BusCMD.PUSH.value if is_push else BusCMD.POP.value
-        port.in_data.value = data
+    @driver_method(match_func=True, need_compare=True)
+    async def opt(self, is_push, data=0):
+        await ClockCycles(self.port, random.randint(0, 5))
+        self.port.in_valid.value = 1
+        self.port.in_cmd.value = BusCMD.PUSH.value if is_push else BusCMD.POP.value
+        self.port.in_data.value = data
 
-        await Value(port.in_ready, 1)
-        port.in_valid.value = 0
+        await Value(self.port.in_ready, 1)
+        self.port.in_valid.value = 0
 
-        port.out_ready.value = 1
-        await Value(port.out_valid, 1)
-        port.out_ready.value = 0
+        self.port.out_ready.value = 1
+        await Value(self.port.out_valid, 1)
+        self.port.out_ready.value = 0
 
-        return port.out_data.value if not is_push else 0
-
-    @driver_method(imme_ret=True, match_func=True, name_to_match="opt", result_compare=True)
-    async def port0_opt(self, is_push, data=0):
-        return await self.exec_once(self.port0, is_push, data)
-
-    @driver_method(imme_ret=True, match_func=True, name_to_match="opt", result_compare=True)
-    async def port1_opt(self, is_push, data=0):
-        return await self.exec_once(self.port1, is_push, data)
+        return self.port.out_data.value if not is_push else 0
 
 class StackModel(Model):
     def __init__(self):
@@ -54,17 +46,21 @@ class StackModel(Model):
 
 async def test_stack(dut):
     mlvp.create_task(mlvp.start_clock(dut))
-
     port0 = StackPortBundle.from_regex("(.*)0(.*)").bind(dut)
     port1 = StackPortBundle.from_regex("(.*)1(.*)").bind(dut)
-    env = StackEnv(port0, port1).attach(StackModel())
+
+    model = StackModel()
+    env1 = StackEnv(port0).attach(model)
+    env2 = StackEnv(port1).attach(model)
 
     for is_push in [True, False]:
         for _ in range(50):
-            await env.port0_opt(is_push, random.randint(0, 2**8-1))
-            await env.port1_opt(is_push, random.randint(0, 2**8-1))
+            await env1.opt(is_push, random.randint(0, 2**8-1))
+            await env2.opt(is_push, random.randint(0, 2**8-1))
 
-    print(await env.drive_completed(sche_method="before_model"))
+    await mlvp.gather(
+        env1.drive_completed(sche_method="before_model"),
+        env2.drive_completed(sche_method="before_model"))
 
 if __name__ == '__main__':
     dut = DUTdual_port_stack()
